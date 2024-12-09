26b29d53f89f59e27c3e58d24fb04861076da27a
ELBERTOPERTUZ <168228624+ELBERTOPERTUZ@users.noreply.github.com>
2024-12-09 16:17:12 -0500

    .

--- beto/Elberto_Pertuz_zapata/Programas/14_21-agosto/bt.java
+++ beto/Elberto_Pertuz_zapata/Programas/14_21-agosto/bt.java
@@ -1,3 +1,19 @@
-public static void main(String[] arg){
-    
+class Beto{
+    public static void main(String[] args) {
+        Tienda bi = new Tienda();
+        bi.setName( "pizza");
+        System.out.println("el restaurante se llama" + bi.getName());
+
+    }
+}
+
+class Tienda{
+    private String name;
+
+    void setName(String n){
+        this.name = n;
+    }
+    String getName(){
+        return this.name;
+    }
 }
